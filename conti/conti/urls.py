edc2a457069fad21fd53087921059cd672b76a2a
VS-DEVELOPER1 <goji49@naver.com>
2017-04-21 17:12:42 +0900

    addtional commit - 2017.04.20

--- conti/conti/urls.py
+++ conti/conti/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.views import defaults
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('contiSvc.urls')),
--- conti/contiSvc/views.py
+++ conti/contiSvc/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, render_to_response
+from django.template import RequestContext
 
 def post_list(request):
         posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,3 +11,16 @@ def post_list(request):
 def post_detail(request, pk):
         post = get_object_or_404(Post, pk=pk)
         return render(request, 'blog/post_detail.html', {'post': post})
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
